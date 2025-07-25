cf3e14a41182a78a6a62a5591401fe629c743a1b
Ecesuuo <orhanecesu@gmail.com>
2025-07-25 12:41:14 +0300

    Güncel proje eklendi

--- app/film/[id]/page.js
+++ app/film/[id]/page.js
@@ -1,7 +1,7 @@
 import FilmDetail from "./FilmDetail";
 import films from "@/app/data/films";
 //routing,props
-export default function FilmDetailPage({ params }) {
+export default async function FilmDetailPage({ params }) {
     const { id } = params;
     const film = films.find(f => f.id === Number(id));
 
